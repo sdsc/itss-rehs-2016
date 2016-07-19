08b6d1d8760af74cebaedae73242bba9f3685d5c
zfhusted <zfhusted@gmail.com>
2016-07-19 11:28:37 -0700

    Working TotalJobs
    TotalJobs arduino file works properly

--- TotalJobs.ino
+++ TotalJobs.ino
@@ -7,18 +7,21 @@ char strValue[MaxChars+1];
 int index = 0;
 int light = 0;
 int color = 0;
-int jobs = 0;
+double jobs = 0;
+int newColor = 0;
+int numLights = 0;
+bool first = true;
 
 void setup() {
   strip.begin();
   //strip.show();
   Serial.begin(9600);
   //strip.setPixelColor(0, strip.Color(255,255,255));
-  
+
 }
 
 void loop() {
- 
+
 }
 
 void serialEvent(){
@@ -31,7 +34,10 @@ void serialEvent(){
       //}
       light = -1;
       color = -1;
+      jobs = 0;
+      first = true;
     }
+
     //Serial.write(ch);
     if(index< MaxChars && isDigit(ch)){
       strValue[index++] = ch;
@@ -39,42 +45,46 @@ void serialEvent(){
     else{
       strValue[index] = 0;
       newColor = atoi(strValue);
-      Serial.print(newColor);
-      if(newColor == 0){
-         numLights = 0;
-         Serial.println(numLights);
+      //Serial.println(newColor);
+      if(first){
+            jobs = newColor*1.0;
+            //Serial.print(jobs);
+            first = false;
       }
       else{
-         numLights = (newColor/jobs)*144;
-         Serial.println(numLights);
+        if(newColor == 0){
+         numLights = 0;
+         //Serial.println(numLights);
+        }
+        else{
+          numLights = (newColor/jobs)*144;
+          Serial.println(numLights);
+        }
       }
-      Serial.print(numLights);
+      //Serial.print(first);
+
       for(int i = 0; i<numLights; i++){
         if(color == 3){
-          strip.setPixelColor(light, strip.Color(0,0,150));
+          strip.setPixelColor(light, strip.Color(150,150,150));
           while(light<144){
-            strip.setPixelColor(light, strip.Color(0,0,150));
+            strip.setPixelColor(light, strip.Color(150,150,150));
             light++;
         }
         }
         else if(color == 2){
-          strip.setPixelColor(light, strip.Color(0,150,0));
+          strip.setPixelColor(light, strip.Color(0,0,150));
         }
         else if(color == 1){
-          strip.setPixelColor(light, strip.Color(150,0,0));
+          strip.setPixelColor(light, strip.Color(150,0,150));
         }
-        else{
-          jobs = newColor;
-        }
- 
-        light++;  
+
+        light++;
       }
       color++;
       strip.show();
       strValue;
       index = 0;
-      
+
     }
   }
 }
-
